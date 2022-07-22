f4c032ae0030d715ba7e4430681bd8648d12ec4f
AbhishekGajera <abhishekgajera3140@gmail.com>
2022-07-23 00:56:27 +0530

    feat : collection section

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Container } from 'react-bootstrap';
+import Collection from './Collection';
 
 const Home = () => {
   return (
     <div>
       <Container>
-        
+          <Collection />
       </Container>
     </div>
   )
