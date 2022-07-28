9aceaab768d4d081093e4ea68fde3df21ccb9d04
avnit007 <avnitnavadiya100@gmail.com>
2022-07-28 22:14:06 +0530

    fix:subHeader

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Container } from 'react-bootstrap';
 import SubHeader from '../SubHeader';
 import Collection from './Collection';
-
 const Home = () => {
   return (
     <div>
