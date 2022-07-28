83bd34df5269df1c2c167df3122e151d529e8456
ApexaBarvaliya <apexabar69@gmail.com>
2022-07-28 15:17:35 -0400

    feat: create footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,128 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Facebook, Instagram, Twitter, Youtube } from 'react-feather';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer-main-block'>
+      <footer className="footer-section">
+        <div className='row newsletter-main-block'>
+          <div className='col-md-3'></div>
+          <div className='col-md-6'>
+            <div className="newslatter-item">
+              <h5>Join Our Newsletter Now</h5>
+              <form action="#" className="subscribe-form">
+                <input type="text" placeholder="Enter Your Mail" />
+                <button type="button">Subscribe</button>
+              </form>
+            </div>
+          </div>
+          <div className='col-md-3'></div>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3 col-sm-6">
+              <div className="footer-item">
+                <h5>Shop</h5>
+                <ul>
+                  <li>Gift cards</li>
+                  <li>Sitemap</li>
+                  <li>Etsy blog</li>
+                  <li>Etsy United Kingdom</li>
+                  <li>Etsy blog</li>
+                  <li>Etsy Canada</li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-md-3 col-sm-6">
+              <div className="footer-item">
+                <h5>Sell</h5>
+                <ul>
+                  <li>
+                    <Link to='/'>Sell on Etsy</Link>
+                  </li>
+                  <li>
+                    <Link to='/'>Teams</Link>
+                  </li>
+                  <li>
+                    <Link to='/'>Forums</Link>
+                  </li>
+                  <li>
+                    <Link to='/'>Affiliates</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-md-3 col-sm-6">
+              <div className="footer-item">
+                <h5>About</h5>
+                <ul>
+                  <li>
+                    <Link to='/'>Policies</Link>
+                  </li>
+                  <li>
+                    <Link to='/'>Investors</Link>
+                  </li>
+                  <li>
+                    <Link to='/'>Careers</Link>
+                  </li>
+                  <li>
+                    <Link to='/'>Press</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-md-3 col-sm-6">
+              <div className="footer-item">
+                <h5>Help</h5>
+                <ul>
+                  <li>
+                    <Link to='/'>Help Center</Link>
+                  </li>
+                  <li>
+                    <Link to='/'>Trust and safety</Link>
+                  </li>
+                  <li>
+                    <Link to='/'>Privacy settings</Link>
+                  </li>
+                </ul>
+              </div>
+              <div className="footer-social">
+                <a href="#">
+                  <Instagram size={20} style={{ color: '#FFFF' }} />
+                </a>
+                <a href="#">
+                  <Facebook size={20} style={{ color: '#FFFF' }} />
+                </a>
+                <a href="#">
+                  <Twitter size={20} style={{ color: '#FFFF' }} />
+                </a>
+                <a href="#">
+                  <Youtube size={20} style={{ color: '#FFFF' }} />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="copyright-reserved">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12">
+                <div className="copyright-text">
+                  Copyright &copy;
+                  <script>document.write(new Date().getFullYear());</script> All
+                  rights reserved | This template is made with{" "}
+                  <i className="fa fa-heart-o" aria-hidden="true"></i> by{" "}
+                  <a href="https://therichpost.com" target="_blank">
+                    Ecommerce
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
   )
 }
 
--- src/components/SubHeader/index.js
+++ src/components/SubHeader/index.js
@@ -18,7 +18,7 @@ const SubHeader = () => {
             <div className="categoriesImgBlock">
               <img
                 src="https://i.etsystatic.com/30213933/c/1510/1510/342/335/il/ea97e0/3755997843/il_300x300.3755997843_9z68.jpg"
-                width="100%"
+                width="100%" alt='img'
               />
             </div>
             <div className="catgoriesTitle">
@@ -30,7 +30,7 @@ const SubHeader = () => {
             <div className="categoriesImgBlock">
               <img
                 src="https://i.etsystatic.com/21433228/r/il/3967dc/3554273629/il_300x300.3554273629_scc8.jpg"
-                width="100%"
+                width="100%" alt='img'
               />
             </div>
             <div className="catgoriesTitle">
@@ -42,7 +42,7 @@ const SubHeader = () => {
             <div className="categoriesImgBlock">
               <img
                 src="https://i.etsystatic.com/31727194/c/1080/1080/0/0/…/0ecb18/3774486627/il_300x300.3774486627_7y0n.jpg"
-                width="100%"
+                width="100%" alt='img'
               />
             </div>
             <div className="catgoriesTitle">
@@ -54,7 +54,7 @@ const SubHeader = () => {
             <div className="categoriesImgBlock">
               <img
                 src="	https://i.etsystatic.com/15411220/c/1056/1056/93/5…/c65b55/1706780060/il_300x300.1706780060_igto.jpg"
-                width="100%"
+                width="100%" alt='img'
               />
             </div>
             <div className="catgoriesTitle">
@@ -66,7 +66,7 @@ const SubHeader = () => {
             <div className="categoriesImgBlock">
               <img
                 src="	https://i.etsystatic.com/10372432/r/il/1b959f/2959473205/il_300x300.2959473205_n35v.jpg"
-                width="100%"
+                width="100%" alt='img'
               />
             </div>
             <div className="catgoriesTitle">
@@ -78,7 +78,7 @@ const SubHeader = () => {
             <div className="categoriesImgBlock">
               <img
                 src="	https://i.etsystatic.com/27502657/r/il/34ea55/2915113681/il_300x300.2915113681_jqpd.jpg"
-                width="100%"
+                width="100%" alt='img'
               />
             </div>
             <div className="catgoriesTitle">
